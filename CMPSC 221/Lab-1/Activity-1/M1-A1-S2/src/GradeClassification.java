02330c6ad93fb04fcfa0997d77a115851fc95a03
Cynid-22 <vunguyenkhang@pm.me>
2025-08-31 20:08:28 -0400

    Activity-1

---
+++ CMPSC 221/Lab-1/Activity-1/M1-A1-S2/src/GradeClassification.java
@@ -0,0 +1,19 @@
+public class GradeClassification {
+    public String letterGrade(int marks) {
+        if (marks < 50) {
+            return "F";
+        } else if (marks <= 60) {
+            return "E";
+        } else if (marks <= 70) {
+            return "D";
+        } else if (marks <= 80) {
+            return "C";
+        } else if (marks <= 90) {
+            return "B";
+        } else if (marks <= 100) {
+            return "A";
+        } else {
+            return "Invalid marks";
+        }
+    }
+}
